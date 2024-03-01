cef0cc55f90f2aaf827ccb84804c2da03339eb37
Arief-IF <ariefichwan2404@gmail.com>
2024-03-01 10:32:35 +0700

    first commit

---
+++ Praktikum1/index.php
@@ -0,0 +1,10 @@
+<?php
+// Membuat Variabel User
+$name = "Arief Ichwan Fanani";
+$age = 19;
+$weight = 77;
+
+echo $name;
+echo "<br>Saya Berusia " . $age;
+
+?>
---
+++ Praktikum2/submit_get.php
@@ -0,0 +1,11 @@
+<?php
+
+$namaLengkap = $_GET['nama_lengkap'];
+$username = $_GET['username'];
+$password = $_GET['password'];
+
+echo "<h1>Selamat {$namaLengkap}, kamu sudah terdaftar</h1>";
+echo "<h3>Username : {$username}</h3>";
+echo "<h3>Password : {$password}</h3>"; 
+
+?>

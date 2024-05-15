d4ed47b149fa69e7b7e50e7e2d1b0429a8fda323
Arief Ichwan Fanani <145969404+Arief-IF@users.noreply.github.com>
2024-05-15 08:23:03 +0700

    web.php

--- puskesmas-new2/routes/web.php
+++ puskesmas-new2/routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/beranda', function () {
+    return "Halaman Beranda Laraval 11";
+});
+
+Route::get('/salam', function () {
+    return "Selamat Datang Arief Ichwan Fanani di Laravel 11 diMatkul Pemrograman Web";
+});
+
+Route::get('/profile', function () {
+    return "Halaman Profile Arief Ichwan Fanani";
+});

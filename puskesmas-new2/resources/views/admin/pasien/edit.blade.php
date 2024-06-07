fc9ed084378d9026575b5d79db14c4b34e8fbf6a
Arief-IF <ariefichwan2404@gmail.com>
2024-06-07 13:43:16 +0700

    Laravel 5

---
+++ puskesmas-new2/resources/views/admin/pasien/edit.blade.php
@@ -0,0 +1,75 @@
+<x-layout>
+    <x-slot name="page_name">Edit Data Pasien</x-slot>
+    <x-slot name="page_content">
+        <form class="forms-sample" action="{{ url('dashboard/pasien/update', $pasien->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="form-group row">
+                <label for="kode" class="col-sm-4 col-form-label">Kode</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control" id="kode" name="kode"
+                        placeholder="Masukkan Kode" value="{{ $pasien->kode }}">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="nama" class="col-sm-4 col-form-label">Nama Pasien</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control" id="nama" name="nama"
+                        placeholder="Masukkan Nama Pasien" value="{{ $pasien->nama }}">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="tmp_lahir" class="col-sm-4 col-form-label">Tempat Lahir</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control" id="tmp_lahir" name="tmp_lahir"
+                        placeholder="Masukkan Tempat Lahir" value="{{ $pasien->tmp_lahir }}">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="tgl_lahir" class="col-sm-4 col-form-label">Tanggal Lahir</label>
+                <div class="col-sm-8">
+                    <input type="date" class="form-control" id="tgl_lahir" name="tgl_lahir"
+                        placeholder="Masukkan Tanggal Lahir" value="{{ $pasien->tgl_lahir }}">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="gender" class="col-sm-4 col-form-label">Jenis Kelamin</label>
+                <div class="col-sm-8">
+                    <select class="form-control" id="gender" name="gender" value="{{ $pasien->gender }}">
+                        <option value="" disabled selected>Masukkan Jenis Kelamin</option>
+                        <option value="L">Laki-laki</option>
+                        <option value="P">Perempuan</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="email" class="col-sm-4 col-form-label">Akun Email</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control" id="email" name="email"
+                        placeholder="Masukkan Akun Email" value="{{ $pasien->email }}">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="alamat" class="col-sm-4 col-form-label">Alamat</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control" id="alamat" name="alamat"
+                        placeholder="Masukkan Alamat" value="{{ $pasien->alamat }}">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="kecamatan_nama" class="col-sm-4 col-form-label">Nama Kecamatan</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control" id="kecamatan_nama" name="kecamatan_nama"
+                        placeholder="Masukkan Nama Kecamatan" value="{{ $pasien->kecamatan_nama }}">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-4"></div>
+                <div class="col-sm-8">
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                    <button type="reset" class="btn btn-warning">Reset</button>
+                </div>
+            </div>
+        </form>
+    </x-slot>
+</x-layout>
